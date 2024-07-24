bbcfb7f65f7422c59206a54373fc44c6e82a3d0a
gv <afairless@gmail.com>
2024-07-24 18:39:59 -0400

    create posts dataframe

--- src/project.py
+++ src/project.py
@@ -4,6 +4,7 @@ import gzip
 import pandas as pd
 import polars as pl
 from pathlib import Path
+from ast import literal_eval
 
 
 def filter_sql_to_correct_table(txt01: list[str]) -> list[str]:
@@ -35,15 +36,31 @@ def filter_sql_to_correct_table(txt01: list[str]) -> list[str]:
     return txt03
 
 
+def create_posts_dataframe(txt: list[str]) -> pd.DataFrame:
+    """
+    Recreate the '_5A5_posts' table from the SQL dump file as a DataFrame
+    """
+
+    colnames_txt = txt[0].split('(')[1].split(')')[0].replace('`', '')
+    colnames = colnames_txt.split(',')
+
+    rows01 = [e for e in txt if e[0] == '(' and 'insert into' not in e.lower()]
+    rows02 = [e[:e.rfind(')')+1] for e in rows01]
+    rows03 = [literal_eval(e) for e in rows02]
+
+    for e in rows03:
+        assert isinstance(e, tuple)
+
+    df = pd.DataFrame(rows03, columns=colnames)
+
+    return df
+
+
 def main():
 
     input_path = Path.cwd() / 'input'
     input_filename = 'localhost.sql.gz'
-    # input_filename = 'localhost.sql'
     input_filepath = input_path / input_filename
-
-    with gzip.open(input_filepath, 'rt') as f:
-        txt = f.read()
 
     txt = []
     with gzip.open(input_filepath, 'rt') as f:
@@ -51,11 +68,16 @@ def main():
             txt.append(line)
 
     txt01 = filter_sql_to_correct_table(txt)
+    df = create_posts_dataframe(txt01)
 
+    output_path = Path.cwd() / 'output'
+    output_filename = 'posts.parquet'
+    output_filepath = output_path / output_filename
+    df.to_parquet(output_filepath)
 
-
-
-
+    output_filename = 'posts.csv'
+    output_filepath = output_path / output_filename
+    df.to_csv(output_filepath)
 
 
 if __name__ == '__main__':
