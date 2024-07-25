0d2aee7ca2c7437265b8dec65700dc54609dd6cc
gv <afairless@gmail.com>
2024-07-25 12:32:26 -0400

    filter to "What I Read/Watch" posts

--- src/s02.py
+++ src/s02.py
@@ -11,11 +11,39 @@ def main():
     input_filepath = input_path / input_filename
     df = pl.read_parquet(input_filepath)
 
-    df.columns
-    cols = range(2, df.shape[1]-2)
-    list(range(2, df.shape[1]-2))
-    cols = [2, 3, 4, 5, 7, 11, 18, 20]
-    df[:, cols]
+    assert (df[:, 2] == df[:, 3]).all()
+    assert (df['post_date'] == df['post_date_gmt']).all()
+
+    # col_idxs = [2, 4, 5, 7, 11, 18, 20]
+
+    # filter by "What I Read/Watch"
+    df2 = df.filter(
+        pl.col('post_type').eq('post') &
+        pl.col('post_title').str.to_lowercase().str.starts_with('what i'))
+
+    # 'post_type' column at index 20 no longer needed
+    col_idxs = [2, 4, 5, 7, 11, 18]
+
+    # for the purpose of needing a single copy of each post, 'inherit' is 
+    #   redundant
+    # 'draft' and 'auto-draft' are also unneeded
+    df3 = df2.filter(
+        ~pl.col('post_status').eq('inherit') &
+        ~pl.col('post_status').str.contains('draft') )
+
+    df2[:, col_idxs]
+    df3[:, col_idxs]
+    df3['post_status'].value_counts()
+    df3['post_title'].n_unique()
+    df3['post_name'].n_unique()
+    df3['post_content'].n_unique()
+    df3['post_type'].value_counts()
+
+
+    # post_status:  future has date when scheduled to be published
+    # post_status:  'publish' apparently same as 'future'
+    # post_status:  inherit has date when scheduled
+
 
 
 
